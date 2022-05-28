1907f6cb555903fed01ea4cde2c79addba7cf5d6
Ali Corrales <alicorrales2013@gmail.comn>
2022-05-27 23:39:29 -0400

    Logging a User Out 10/22

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local').Strategy;
 
-
+//Middleware Para verificar si esta autenticado
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
       return next();
@@ -82,6 +82,15 @@ myDB(async client => {
   app.route('/profile').get(ensureAuthenticated, (req,res) => {
     res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
  });
+
+ app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+app.use((req, res, next) => {
+  res.status(404).type('text').send('Not Found');
+});
 
   app.route('/chat').get((req, res) => {
     res.render(process.cwd() + '/views/pug/chat');
