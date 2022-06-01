610a0b7c94b52401f90c94b654dd6db2d253c7b1
Ali Corrales <73853235+alicorrales7@users.noreply.github.com>
2022-06-01 15:34:15 -0400

    16/22 Implementation final of social Authentication

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
+require('dotenv').config();
 const bcrypt = require('bcrypt');
 
 module.exports = function (app, myDataBase) {
@@ -28,5 +30,39 @@ module.exports = function (app, myDataBase) {
           });
         }
       ));
-
+    
+      passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.CALL_BACK
+      },
+        function(accessToken, refreshToken, profile, cb) {
+          console.log(profile);
+          myDataBase.findOneAndUpdate(
+            { id: profile.id },
+            {
+              $setOnInsert: {
+                id: profile.id,
+                name: profile.displayName || 'John Doe',
+                photo: profile.photos[0].value || '',
+                email: Array.isArray(profile.emails)
+                  ? profile.emails[0].value
+                  : 'No public email',
+                created_on: new Date(),
+                provider: profile.provider || ''
+              },
+              $set: {
+                last_login: new Date()
+              },
+              $inc: {
+                login_count: 1
+              }
+            },
+            { upsert: true, new: true },
+            (err, doc) => {
+              return cb(null, doc.value);
+            }
+          );
+        }
+      ));
 };
